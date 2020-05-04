a1839334953304932b5aada0f89ce8346de74e8f
drholix <drholix@gmail.com>
2020-05-04 17:06:56 +0700

    Penambahan Button Perpindahan Activity Dengan Data.

--- app/src/main/java/id/awan/myapplication/MainActivity.java
+++ app/src/main/java/id/awan/myapplication/MainActivity.java
@@ -16,15 +16,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnMoveActivity = findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
+
+        Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
+        btnMoveWithDataActivity.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v){
-        switch (v.getId()){
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.btn_move_activity:
                 Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
                 startActivity(moveIntent);
-            break;
+                break;
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Alief Kurniawan");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 21);
+                startActivity(moveWithDataIntent);
+                break;
         }
     }
 }
